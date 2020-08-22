97590af2726fd2405151847be79df47dd73766a5
David Darnes <me@daviddarnes.com>
2020-08-22 17:48:41 +0100

    Add CSS to eleventy build

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("styles.css");
 };
