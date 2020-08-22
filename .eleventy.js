7a26d0f1c67ffb31849591adfc1d35ebae5eb99e
David Darnes <me@daviddarnes.com>
2020-08-22 17:53:42 +0100

    Update eleventy usage for more simple project structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("styles.css");
 };
