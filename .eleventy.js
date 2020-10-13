00e505d01c7d57e771e95b63f214ca6b6793181d
David Darnes <me@daviddarnes.com>
2020-10-13 12:44:24 +0100

    Refine API call to get blog data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,22 @@
-module.exports = function(eleventyConfig) {
+const fetch = require("node-fetch");
+
+async function getPosts() {
+  return await fetch(
+    "https://api.dropinblog.com/v1/json/?b=MU9HVOP48VXAY2ITW4FZ&includecontent=1"
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      return json.data.posts;
+    });
+}
+
+module.exports = function (eleventyConfig) {
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Create Collection from DropInBlog posts
+  eleventyConfig.addCollection("posts", async function (collection) {
+    collection = await getPosts();
+    return collection;
+  });
 };
